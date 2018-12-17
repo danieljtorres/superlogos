b62c558e5826c194e1fc61d24b9ac1340749a125
Daniel Torres <danieljtorres94@gmail.com>
2018-12-17 20:40:15 -0300

    .

--- .nuxt/toast.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import Toasted from 'vue-toasted'
-
-Vue.use(Toasted, {"position":"top-right","duration":2000,"className":"toast","singleton":true})
-
-export default function (ctx, inject) {
-  inject('toast', Vue.toasted)
-}

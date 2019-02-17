f8cf7ab2dc1aaef05da5c68b78b48a0c9c7649f2
XEarias <xarias13@gmail.com>
2019-02-17 16:14:14 -0300

    servicios de rotulos y social media

--- src/pages/redes-sociales.vue
+++ src/pages/redes-sociales.vue
@@ -14,7 +14,7 @@
           <v-layout row wrap>
             <v-flex v-for="(wpayf, index) in wePutAtYourFingertips" :key="index" md4 class="mb-3 pa-1">
               <div class="text-xs-center pa-3" style="border-bottom: 3px solid #2196F3; background-color: rgba(0, 0, 0, 0.75);">
-                <img :src="require(`~/assets/images/logotipos/icons/work_table_${index + 1}.png`)" width="30%" style="filter: brightness(10);">
+                <img :src="`~/assets/images/logotipos/icons/work_table_${index + 1}.png`" width="30%" style="filter: brightness(10);">
                 <br>
                 <h2 class="body-2 font-weight-bold text-xs-center mb-2" style="color: white;">{{ wpayf.title }}</h2>
                 <p class="caption" style="color: white;">{{ wpayf.text }}</p>
@@ -41,6 +41,19 @@
             </v-flex>
           </v-layout>
         </v-container>
+      </v-flex>
+    </v-layout>
+    <v-layout row class="mt-5">
+      <v-flex offset-md2 md8>
+        <AppHeading number="2" size="headline" color="#000000" title="PLANES DISPONIBLES"/>
+        <AppServiceBox v-for="(service, index) in services" :key="index"
+          :title="service.name"
+          :description="service.description"
+          :price="service.price.value + ' ' + service.price.symbol"
+          :img="service.image"
+          :slug="service.slug"
+          :items="service.items"
+        />
       </v-flex>
     </v-layout>
     <v-layout row class="mt-5">
@@ -78,10 +91,64 @@
           { title: '+40 PROFESIONALES', text: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum' },
           { title: 'DISEÑOS DE UN DÍA PARA OTRO', text: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum' },
           { title: 'ATENCIÓN PERZONALIZADA', text: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum' }
+        ],
+        services: [
+          {
+            name: 'EMPRENDEDOR',
+            price: {
+              value: 170.00,
+              symbol: '$'
+            },
+            description: '',
+            items: [
+              '1 Redes sociales (Facebook, Instagram o Twitter).',
+              'Diseño simple de portadas.',
+              '1 Publicación diaria (por red social).',
+              'Interacción continua y estión de la reputación.',
+              'Diseño de publicaciones y programación de contenido.',
+              'Informe trimestral de estadísticas.',
+              'Bonus 10 usd (publicidad Facebook ads).'
+            ],
+            image: `/images/social-media/packages/1.png`
+          },
+          {
+            name: 'EMPRESARIAL',
+            price: {
+              value: 250.00,
+              symbol: '$'
+            },
+            description: '',
+            items: [
+              '2 Redes sociales (Facebook, Instagram o Twitter).',
+              'Diseño personalizado de portadas.',
+              '2 Publicación diarias.',
+              'Interacción continua y gestión de la reputación.',
+              'Plan de redes, diseño de publicaciones y programación de contenido.',
+              'Informe trimestral.',
+              'Atención al cliente (respuesta inmediata).',
+              'Bonus 10 usd (publicidad Facebook ads).'
+            ],
+            image: `/images/social-media/packages/2.png`
+          },
+          {
+            name: 'EMPRESARIAL PREMIUM',
+            price: {
+              value: 399.00,
+              symbol: '$'
+            },
+            description: '',
+            items: [
+              '3 Redes sociales (Facebook, Instagram y Twitter).',
+              'Diseño personalizado de portadas.',
+              '2 Publicación diarias.',
+              'Estrategia de redes, diseño de publicaciones y programación de contenido.',
+              'Atención al cliente (respuesta inmediata).',
+              'Creación de estrategias plataformas pagas.'
+            ],
+            image: `/images/social-media/packages/3.png`
+          }
         ]
       }
-    },
-    mounted () {
     }
   }
 </script>
--- src/pages/rotulos.vue
+++ src/pages/rotulos.vue
@@ -8,7 +8,7 @@
           <v-layout row wrap>
             <v-flex v-for="(wpayf, index) in wePutAtYourFingertips" :key="index" md3 class="mb-3 pa-1">
               <div class="text-xs-center pa-3" style="border-bottom: 3px solid #2196F3; background-color: rgba(0, 0, 0, 0.75);">
-                <img :src="require(`~/assets/images/rotulados/icons/${index + 1}.png`)" width="30%" style="filter: brightness(10);">
+                <img :src="`~/assets/images/rotulados/icons/${index + 1}.png`" width="30%" style="filter: brightness(10);">
                 <br>
                 <h2 class="body-2 font-weight-bold text-xs-center mb-2" style="color: white;">{{ wpayf.title }}</h2>
               </div>
@@ -34,6 +34,19 @@
             </v-flex>
           </v-layout>
         </v-container>
+      </v-flex>
+    </v-layout>
+    <v-layout row class="mt-5">
+      <v-flex offset-md2 md8>
+        <AppHeading number="2" size="headline" color="#000000" title="¿QUE ESTÁ BUSCANDO?"/>
+        <AppServiceBox v-for="(service, index) in services" :key="index"
+          :title="service.name"
+          :description="service.description"
+          :price="service.price.value + ' ' + service.price.symbol"
+          :img="service.image"
+          :slug="service.slug"
+          :items="service.items"
+        />
       </v-flex>
     </v-layout>
     <v-layout row class="mt-5">
@@ -69,10 +82,37 @@
           { title: 'GARANTIA DE CONFORMIDAD' },
           { title: 'DISEÑOS ILIMITADOS' },
           { title: '+40 PROFESIONALES' }
+        ],
+        services: [
+          {
+            name: 'ROTULADO VEHICULAR',
+            price: {
+              value: 99.00,
+              symbol: '$'
+            },
+            description: 'Diseñamos el rótulo ideal para su vehículo, que lo haga dejar huella a donde quiera que vaya. Dele identidad a su auto. Este paquete incluye el diseño de rotulado para un solo modelo de auto.',
+            image: `/images/rotulos/packages/1.png`
+          },
+          {
+            name: 'ROTULADO PARA VAN',
+            price: {
+              value: 109.00,
+              symbol: '$'
+            },
+            description: 'Creamos la imagen ideal para su van. Reciba el diseño de rotulado de su vehículo de transporte. Ideal para nuevas empresas. Este paquete incluye el diseño de rótulo para un solo modelo de van.',
+            image: `/images/rotulos/packages/2.jpg`
+          },
+          {
+            name: 'ROTULADO PARA FLOTA',
+            price: {
+              value: 199.00,
+              symbol: '$'
+            },
+            description: '¡Es momento de que su empresa se haa notar en las calles! Influye el diseño de rotulos para cuatro modelos de vehículo, diseños distintos para cada unidad, todos bajo  la estética de su imagen como su marca',
+            image: `/images/rotulos/packages/3.png`
+          }
         ]
       }
-    },
-    mounted () {
     }
   }
 </script>
